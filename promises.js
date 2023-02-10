d830c9d1e8e8306f4609f11823bf82f5b90afd2e
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2023-02-09 22:03:03 -0500

    promises

--- promises.js
+++ promises.js
@@ -19,5 +19,37 @@ document.forEach(function(documentName) {
 function getUserConfirmation() {
     let modalDiv = document.createElement('div')
 
+    modalDiv.style.padding = '10px';
+    modalDiv.style.position = 'absolute';
+    modalDiv.style.backgroundColor = 'white';
+    modalDiv.style.border = '1px solid balck';
+    modalDiv.style.width = '50%';
+    modalDiv.style.height = '100px';
+    modalDiv.style.left = '20%';
+    modalDiv.style.top = '20px';
+
+
+    let modalHeaderDiv = document.createElement('div');
+    modalHeaderDiv.textContent = 'Are you sure you want to delete this document?'
+
+    modalDiv.append(modalHeaderDiv)
+
+    let confirmButton = document.createElement('button');
+    confirmButton.textcontent = 'Confirm'
+
+    let cancelButton = document.createElement('button');
+    cancelButton.textcontent = 'Cancel'
+
+    modalDiv.append(confirmButton, cancelButton)
+
+    confirmButton.addEventListener('click', function() {
+
+    })
+
+    cancelmButton.addEventListener('click', function() {
+        
+    })
+
+    document.body.append(modalDiv)
 
 }
