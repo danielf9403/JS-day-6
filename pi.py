9e0642063807ba1e50e2d61f818c9eff48149c47
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2023-02-11 00:52:33 -0500

    some python practice

--- pi.py
+++ pi.py
@@ -1,3 +1,4 @@
+
 from bs4 import BeautifulSoup
 import requests
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029 Safari/537.36'}
@@ -5,4 +6,15 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 def weathee(city):
     city = city.replace("","+")
     res = requests.get(f'https://www.google.com/search?p={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8',headers=heater)
-print("Searching in google......\n")      
+print("Searching in google......\n")
+
+soup = BeautifulSoup(res.text,'html.parser')
+location = soup.select('#wob_loc')[0].getText().strip()
+time = soup.select('#wob_dts')[0].getText().strip()
+info = soup.select('#wob_dc')[0].getText().strip()
+weather = soup.select('#wob_tm')[0].getText().strip()
+
+print(location)
+print(time)
+print(info)
+print(weather+"°C")
